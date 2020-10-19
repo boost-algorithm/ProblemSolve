ca8d81f91b78e4088938ef77d2442920885275f5
do02reen24 <do02reen24@gmail.com>
2020-10-19 12:28:21 +0900

    unsolved : baekjoon 18233
    - 86%에서 틀림

--- src/do02reen24/bit-masking/baekjoon_18233.cpp
+++ src/do02reen24/bit-masking/baekjoon_18233.cpp
@@ -1,20 +1,3 @@
-# :fire: week7
-
-## :ballot_box_with_check: 백준 1094
-
-* list를 사용하지 않고 풀고 싶었는데 잘안돼서 그냥 list로 풀었다.
-* 스터디 고찰: 비트마스크를 써서 풀면 코드가 훨씬 간결함을 알게 되었다.
-
-## :ballot_box_with_check: 백준 14889
-
-* c++의 permutation을 쓰면 쉬울 것 같아 c++로 해결하였다.
-* 스터디 고찰 : python의 경우 combination을 써서 풀 수 있음을 알게 되었다.
-
-## :ballot_box_with_check: 백준 18233
-
-#### 틀렸습니다. (86%)
-
-```c++
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -46,12 +29,12 @@ int main() {
                 if (isGift[i] == 0) continue;
                 int diff = member[i].second - member[i].first;
                 if (diff < remain) { 
-                    remain -= diff; 
+                    remain -= diff;
                     sol[i] += diff;
                 }
                 else {
                     sol[i] += remain;
-                    break;
+                    remain = 0;
                 }
             }
             for (int i = 0; i < n; i++) cout << sol[i] << " ";
@@ -61,7 +44,3 @@ int main() {
     cout << -1 << endl;
     return 0;
 }
-```
-
-## :ballot_box_with_check: 백준 1194
-
