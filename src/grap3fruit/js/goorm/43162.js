d1c9fff17bfe696a6c60785f8dce75ed811a20a3
grap3fruit <swkwon333@gmail.com>
2021-01-05 21:46:01 +0900

    15주차 문제풀이

---
+++ src/grap3fruit/js/goorm/43162.js
@@ -0,0 +1,54 @@
+const bfs = (computers, root) => {
+  const visited = [root];
+  let q = [computers[root]];
+
+  while (q.length > 0) {
+    let item = q.shift();
+
+    item.forEach((el, idx) => {
+      if (el === 1 && !visited.includes(idx)) {
+        q.push(computers[idx]);
+        visited.push(idx);
+      }
+    });
+  }
+
+  return visited;
+};
+
+function solution(n, computers) {
+  let answer = 0;
+  const visited = [];
+
+  computers.forEach((_, idx) => {
+    let visitFlag = false;
+    visited.forEach((el) => {
+      if (el.includes(idx)) {
+        visitFlag = true;
+      }
+    });
+
+    if (!visitFlag) {
+      const newVisited = bfs(computers, idx);
+      visited.push(newVisited);
+    }
+  });
+
+  console.log(visited.length);
+  answer = visited.length;
+  return answer;
+}
+
+const n = 3;
+const computers = [
+  [1, 1, 0],
+  [1, 1, 0],
+  [0, 0, 1],
+];
+// const computers = [
+//   [1, 1, 0],
+//   [1, 1, 1],
+//   [0, 1, 1],
+// ];
+
+solution(n, computers);
